11950c48f71799eeb85c2bd52bb6146d43754af0
Collin <wildkard91@gmail.com>
2022-06-08 10:36:14 -0400

    created a nav skeleton component for the dashbaord

---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+<header>
+    <div class="nav_container"> 
+        this will be the nav bar
+    </div>
+</header>
+</template>
+
+<script>
+export default{
+    name: 'Nav',
+    props: {},
+    methods: {},
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="dashboard">
+    <Nav />
     <h1>This is a dashboard that will display the wallets</h1>
   </div>
 </template>
 
 
 <script>
+import Nav from '@/components/Nav'
+
+export default {
+  name: 'DashboardView',
+  components: {
+    Nav
+  }
+}
 </script>
