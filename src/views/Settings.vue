63b5da78839028a5301633b93e17506723d1e730
Collin <wildkard91@gmail.com>
2022-06-08 13:53:51 -0400

    fleshed out skeleton for settings page with dummy items

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,82 @@
 <template>
-<Nav/>
-<h1>This will be the settings page</h1>
+<div class="page"></div>
+    <Nav/>
+      <div class="dashboard">
+        <div class="head_container">
+          <h1>Settings</h1>
+        </div>
+
+        <div class="settings_container">
+          <div class="item_container_left">
+          <h2>Security</h2>
+          </div>
+          <div class="item_container_right">
+            <h2>Label</h2>
+            <span><img src="@/assets/carat_right.png"/></span>
+          </div>
+        </div>
+
+        <div class="settings_container">
+                <div class="item_container_left">
+                <h2>Fiat Currency</h2>
+                </div>
+                <div class="item_container_right">
+                    <h2>$ USD</h2>
+                    <span><img src="@/assets/carat_right.png"/></span>
+                </div>
+                </div>
+
+        <div class="settings_container">
+                <div class="item_container_left">
+                <h2>Bitcoin Units</h2>
+                </div>
+                <div class="item_container_right">
+                    <h2>Sat, BTC</h2>
+                    <span><img src="@/assets/carat_right.png"/></span>
+                </div>
+                </div>
+
+        <div class="settings_container">
+                <div class="item_container_left">
+                <h2>Network</h2>
+                </div>
+                <div class="item_container_right">
+                    <h2>Mainnet</h2>
+                    <span><img src="@/assets/carat_right.png"/></span>
+                </div>
+                </div>
+
+        <div class="settings_container">
+                <div class="item_container_left">
+                <h2>Notifications</h2>
+                </div>
+                <div class="item_container_right">
+                    <h2></h2>
+                    <span><img src="@/assets/carat_right.png"/></span>
+                </div>
+                </div>
+
+        <div class="settings_container">
+                <div class="item_container_left">
+                <h2>Language</h2>
+                </div>
+                <div class="item_container_right">
+                    <h2>English</h2>
+                    <span><img src="@/assets/carat_right.png"/></span>
+                </div>
+                </div>      
+
+        <div class="settings_container">
+                <div class="item_container_left">
+                <h2>Dark Mode</h2>
+                </div>
+                <div class="item_container_right">
+                    <h2>Disabled</h2>
+                    <span><img src="@/assets/carat_right.png"/></span>
+                </div>
+                </div>
+
+      </div>
 </template>
 
 <script>
@@ -13,3 +89,47 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.dashboard{
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  width:50%;
+}
+h1{
+  margin-top: 50px;
+  padding:10px;
+}
+.head_container{
+  border-style: solid; 
+  border-width: 0px 0px 1px 0px;
+  border-color: #DEDEDE;
+  width: 80%;
+}
+.settings_container{
+  display:flex;
+  flex-direction: row;
+  border-style: solid; 
+  border-width: 0px 0px 1px 0px;
+  border-color: #DEDEDE;
+  width: 80%;
+}
+.item_container_left{
+  display:flex;
+  flex-direction: column;
+  align-items:flex-start;
+  width:50%;
+}
+.item_container_right{
+  display:flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  align-items:center;
+  align-content:center;
+  width:50%;
+}
+</style>
