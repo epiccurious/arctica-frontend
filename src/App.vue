be1a3262c0644f1048136fa0df1691927b5fdcbf
Collin <wildkard91@gmail.com>
2022-06-08 10:16:31 -0400

    changed router links to a more dynamic format

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="{ name: 'login' }"">Home</router-link> |
+    <router-link to="{ name: 'about' }">About</router-link>
   </nav> -->
   <!-- The above nav and router links will be reworked for post login -->
   <router-view/>
