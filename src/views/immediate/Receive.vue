dfb2e48d40ee1c7c1323907038e82361f957f5d7
Collin <wildkard91@gmail.com>
2022-06-10 13:00:15 -0400

    added some template elements with placeholders and styling to immediate receive page

--- src/views/immediate/Receive.vue
+++ src/views/immediate/Receive.vue
@@ -1,22 +1,47 @@
 <template>
 <div class="page">
     <NavImmediate/>
-    <h1>This is the receive page</h1>
+    <Header title="Receive Bitcoin"/>
+        <div class="receive_container">
+            <div class="receive_top">
+                <img src="@/assets/placeholderQR.png">
+                <h2 id="receive_address">bc1qyfgj82tfxndmjl237j6xdvvhxrrnfky</h2>
+            </div>
+        </div>
 </div>
 </template>
 
 
 <script>
 import NavImmediate from '@/components/NavImmediate'
+import Header from '@/components/Header'
 
 export default {
   name: 'immediateReceive',
   components: {
-    NavImmediate
+    NavImmediate,
+    Header
   },
 }
 </script>
 
 
 <style>
+.receive_container{
+    border: 1px solid #EDEDED;
+    border-radius: 5px;
+    width: 450px;
+}
+.receive_top{
+    display:flex;
+    flex-direction: row;
+}
+#receive_address{
+    font-weight:400;
+    font-size:16px;
+    line-height: 20px;
+    text-transform: uppercase;
+    font-feature-settings: 'tnum' on, 'lnum' on, 'zero' on;
+    color: #777777
+}
   </style>
