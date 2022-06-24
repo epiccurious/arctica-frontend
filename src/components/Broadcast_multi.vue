0ce834de3bf1f109b743cd5fbb503a111483fdf2
Collin <wildkard91@gmail.com>
2022-06-24 12:02:38 -0400

    created seperate components for broadcast and signing to display multiple outputs

---
+++ src/components/Broadcast_multi.vue
@@ -0,0 +1,135 @@
+<!-- This component should only show for transactions with more than one output! -->
+
+<template>
+<Confirmation v-if="sent" />
+<div v-else class="page">
+    <!-- <div id="back_button">
+        <button class="btnclose" @click="$emit('closeOut')"><img src="@/assets/carat_left.png">Back</button>
+    </div> -->
+    <div class="display_block">
+        <h1>Ready to send?</h1>
+        <img src="@/assets/checkmark_green.png">
+        <div class="tx_block">
+            <h2>To</h2>
+            <h3>{{ this.transaction.address }}</h3>
+        </div>
+
+        <div class="tx_block">
+            <h2>Amount</h2>
+            <h3>₿ {{ this.transaction.amount }}</h3>
+        </div>
+
+        <div class="tx_block">
+            <h2>Fee</h2>
+            <h3>₿ {{ this.transaction.fee }}</h3>
+        </div>
+
+        <div id="btn_container">
+            <button @click="broadcast(this.transaction)" class="btn">Send</button>
+            <button @click="$emit('closeOut')" class="btn2">Discard</button>
+        </div>
+        
+
+    </div>
+</div>
+</template>
+
+
+<script>
+import Confirmation from '@/components/Confirmation'
+
+export default {
+    props: ['transaction'],
+    components: {
+        Confirmation
+    },
+    methods: {
+        broadcast(){
+            console.log('broadcasting...')
+            this.sent = true
+        }
+    },
+    data(){
+        return{
+        sent: false
+        }
+    },
+}
+</script>
+
+<style scoped>
+.display_block{
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    padding: 4%;
+    width: 40%;
+}
+#btn_container{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: center;
+    justify-items:center;
+    justify-content: center;
+    margin-top: 2%;
+    gap: 21px;
+}
+.tx_block{
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-content:center;
+    width:90%;
+    border-style: solid; 
+    border-width: 0px 0px 1px 0px;
+    border-color: #DEDEDE;
+}
+#back_btn{
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    align-content: center;
+    justify-items: center;
+    justify-content: center;
+}
+.btn{
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+.btnclose{
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    align-content: center;
+    font-weight:500;
+    font-size:18px;
+    line-height: 18px;
+}
+h3{
+    font-family: 'SF Pro Display';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 140%;
+}
+h2{
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 140%;
+}
+h1{
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 21px;
+    line-height: 140%;
+}
+
+</style>
