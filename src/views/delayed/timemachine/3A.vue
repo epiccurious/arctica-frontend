4d5b62ae6266a0f8b49b9694d8a93872aa16f459
Collin <wildkard91@gmail.com>
2022-06-27 12:48:07 -0400

    added a time machine status update screen for the delayed wallet tab

---
+++ src/views/delayed/timemachine/3A.vue
@@ -0,0 +1,30 @@
+<!-- This screen will automatically show when the user navigates to the delayed wallet while they have an open Time Machine Request -->
+<!-- This screen is currently not displayed anywhere -->
+
+<template>
+  <div class="login">
+<Header title="Your Time Machine request is still processing" subtitle="We are still waiting on Time Machine Operators. Check back in 24 hours."/>
+    <div class="btn_container"> 
+        <button @click="acknowledge()" class="btn">Ok</Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  name: 'TimeMachine4',
+  components: {
+    Header,
+    },
+    methods: {
+        acknowledge(){
+            console.log('user ack')
+            this.$router.push({ path: '/wallets' })
+        },
+
+    },
+}
+</script>
+
