ccb9dae8c144e692911ef14aeeeff076da967852
Collin <wildkard91@gmail.com>
2022-06-28 10:18:39 -0400

    added a lockout screen for when the user has 5 failed login attempts

---
+++ src/views/Manual_Login/BPS_Bricked.vue
@@ -0,0 +1,58 @@
+<!-- This page will be a popup that appears if the user has 5 unsuccesful login attempts and Bricks the BPS relationship-->
+
+<template>
+<div class="page">
+    <header>
+        <h1>You have 5 failed login attempts.</h1>
+        <h2>Arctica will now initialize the manual recovery process.</h2>
+        <h2>This will require you access additional SD cards.</h2>
+    </header>
+    
+
+        <div class="btn_container"> 
+            <button @click="acknowledge()" class="btn">Ok</Button>
+        </div>
+
+
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'TimeMachine9',
+  components: {
+    },
+    methods: {
+        acknowledge(){
+            console.log('user ack, closing popup')
+            this.$router.push({ path: '/wallets/delayed' })
+            //update a global delayed wallet unlocked variable here
+        },
+
+    },
+}
+</script>
+
+<style scoped>
+header{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px 0px 25px;
+    margin: 0 auto;
+    margin-top: 5%;
+}
+h1{
+    font-size: 21px;
+    font-weight:600;
+    line-height: 25px;
+}
+h2{
+    font-size: 18px;
+    color: #777777;
+    font-weight: 800;
+}
+</style>
