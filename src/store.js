e9891cb967793a1969d939f1e0b49e12d585875e
Collin <wildkard91@gmail.com>
2022-07-06 09:50:29 -0400

    added global state variables for bootup greenlight

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { createStore } from 'vuex'
 
 export default createStore({
     state: {
+    //bootup checking for special conditions and allowing for login
+    psbtFound:null, btcCoreHealthy:null, bpsHealthy:null, tripwireHealthy:null, timeMachineKeyFound:null, privacyKeyFound:null,
     //data for creating a new Transaction
      id:null, description:null, address:null, amount:null, fiat_currency:null, datetime:null, fee:null, customFee:null, status: null,
-    //fetching existing transactions, placeholder hardcodes
+    //existing transaction history, placeholder hardcodes
       immediateTransactions: [
         {id: 1, address: 'bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh', balance: 21736, fiat_currency: 12.75, datetime: '2022-06-07T12:00:00-11:01', fee: 0.00000987, status: 'Confirmed'},
         {id: 2, address: 'bc1qu7cr0hyc4xfnk3fh0cdce43fnzfwdtq5a089vs3pp7d0hhxesvwq88ecfp', balance: 29345, fiat_currency: 15.28, datetime: '2022-06-09T12:00:00-09:30', fee: 0.00001247, status: 'Confirmed'},
@@ -95,6 +97,25 @@ export default createStore({
         getDelayedBalance(state){
             return state.delayedBalance
         },
+        getPSBTFound(state){
+            return state.psbtFound
+        },
+        getBTCCoreHealthy(state){
+            return state.btcCoreHealthy
+        },
+        getBPSHealthy(state){
+            return state.bpsHealthy
+        },
+        getTripwireHealthy(state){
+            return state.tripwireHealthy
+        },
+        getTimeMachineKeyFound(state){
+            return state.timeMachineKeyFound
+        },
+        getPrivacyKeyFound(state){
+            return state.privacyKeyFound
+        },
+
 
     }
 })
