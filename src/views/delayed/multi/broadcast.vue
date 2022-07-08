d94ed4f72e45d520275732bd499dc5debb63957b
Collin <wildkard91@gmail.com>
2022-07-08 10:33:45 -0400

    added broadcast and confirmation screens to delayed spend flow

---
+++ src/views/delayed/multi/broadcast.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="page">
+    <div class="display_block">
+        <h1>Ready to send?</h1>
+        <img src="@/assets/checkmark_green.png">
+        <div class="tx_block">
+            <h2>To</h2>
+            <h3>{{ this.transaction.address }}</h3>
+        </div>
+
+        <div class="tx_block">
+            <h2>Amount</h2>
+            <h3>₿ {{ this.transaction.amount }}</h3>
+        </div>
+
+        <div class="tx_block">
+            <h2>Fee</h2>
+            <h3>₿ {{ this.transaction.fee }}</h3>
+        </div>
+
+        <div class="horizontal_btn_container">
+            <button @click="broadcast(this.transaction)" class="btn">Send</button>
+            <button @click="discard()" class="btn2">Discard</button>
+        </div>
+        
+
+    </div>
+</div>
+</template>
+
+
+<script>
+import store from '../../../store.js'
+
+export default {
+    name: 'immediateBroadcast',
+    components: {},
+    methods: {
+        broadcast(){
+            console.log('broadcasting...')
+            this.$router.push({ name: 'immediateConfirmation' }) 
+            store.commit('pushImmediateTransaction', this.transaction)
+        },
+        discard(){
+            console.log('discarding PSBT')
+            store.commit('clearTransaction')
+            this.$router.push({ name: 'immediate' }) 
+        },
+    },
+    computed:{
+        transaction(){
+            return store.getters.getTransaction
+        }
+    }
+}
+</script>
