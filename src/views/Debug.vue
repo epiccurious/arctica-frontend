7d0e6d65d2086dab1dd61e2b9dca15c3287e5aa3
Collin <wildkard91@gmail.com>
2022-07-11 09:57:03 -0400

    styled toggle buttons on the debug page, added placeholder buttons to toggle global state

--- src/views/Debug.vue
+++ src/views/Debug.vue
@@ -5,10 +5,40 @@
         <h2>This is for testing only.</h2>
     </header>
     <div class="btn_container"> 
-        <label for="toggleswitch" class="custom_control_label">Test</label>
-        <input type="checkbox" class="custom_control_input" id="toggleswitch">
-    </div>
-    
+
+        <div class="switch">
+            Initial Setup Complete
+            <label class="toggle_switch_label">
+                <input type="checkbox" checked>
+                <span class="slider"></span>
+            </label>
+        </div>
+
+        <div class="switch">
+            Post Setup Complete
+            <label class="toggle_switch_label">
+                <input type="checkbox" checked>
+                <span class="slider"></span>
+            </label>
+        </div>
+
+        <div class="switch">
+            Tripwire Healthy
+            <label class="toggle_switch_label">
+                <input type="checkbox" checked>
+                <span class="slider"></span>
+            </label>
+        </div>
+
+        <div class="switch">
+            Delayed Wallet Timelocked
+            <label class="toggle_switch_label">
+                <input type="checkbox" checked>
+                <span class="slider"></span>
+            </label>
+        </div>
+
+    </div> 
 </div>
 </template>
 
@@ -16,16 +46,58 @@
 export default{
     name: 'Debug',
 
-    methods:{
-        goHome(){
-            this.$router.push({ name: 'dashboard' })
-        }
-    }
+    methods:{},
 
 }
 
 </script>
 
-<style>
+<style scoped>
+.toggle_switch_label{
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+}
+.toggle_switch_label input{
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+.slider{
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 34px;
+}
+.slider:before{
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 50%;
+}
 
+input:checked + .slider{
+    background-color:#F7931A;
+}
+input:focus + .slider{
+    box-shadow: 0 0 1px #F7931A;
+}
+input:checked + .slider:before{
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+}
 </style>
