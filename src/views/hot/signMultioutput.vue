c48764f4b63c8db95990d6417217e875e38bdff4
Collin <wildkard91@gmail.com>
2022-08-01 09:54:16 -0400

    for real this time, fixed a bug in the discard button on multi output send screens

--- src/views/hot/signMultioutput.vue
+++ src/views/hot/signMultioutput.vue
@@ -56,6 +56,7 @@ export default {
         discard(){
             console.log('discarding PSBT')
             store.commit('clearTransaction')
+            store.commit('clearPSBTArr')
             this.$router.push({name: 'hot'}) 
         },
         forward(){
