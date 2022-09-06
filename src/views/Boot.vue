2c4d26a1d84db571a3a512af87fabcc42e0cf94a
lappy <wildkard91@gmail.com>
2022-09-06 11:06:54 -0400

    testing cursor animations

--- src/views/Boot.vue
+++ src/views/Boot.vue
@@ -44,14 +44,14 @@ export default {
             console.log('fetching help')
         },
         async install(){
-          document.body.style.cursor = 'wait'
+          document.page.style.cursor = 'wait'
           //begin install
           //obtain latest tails image
           const invoke = window.__TAURI__.invoke
           await setTimeout(invoke('obtain_tails').then((response) => console.log(response)), 1000)
           invoke('print_rust', {data: 'inputed data'}).then((response) => console.log(response))
 
-          document.body.style.cursor = 'default'
+          document.page.style.cursor = 'default'
           
           //need to await a response that download is complete here before proceeding to below
           this.$router.push({ name:'Setup1' })
