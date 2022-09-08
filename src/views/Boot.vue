0f80e77c49536650aa67c726b5b7775dd3178639
lappy <wildkard91@gmail.com>
2022-09-08 13:37:30 -0400

    setting timeout to 10s

--- src/views/Boot.vue
+++ src/views/Boot.vue
@@ -48,7 +48,7 @@ export default {
           //begin install
           //obtain latest tails image
           const invoke = window.__TAURI__.invoke
-          await setTimeout(invoke('obtain_tails').then((response) => console.log(response)), 1000)
+          await setTimeout(invoke('obtain_tails').then((response) => console.log(response)), 10000)
           invoke('print_rust', {data: 'inputed data'}).then((response) => console.log(response))
 
           document.body.style.cursor = 'default'
