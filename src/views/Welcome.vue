2a9a234bdde6bf78cd76c87bd4676bad83a9cb6c
lappy <wildkard91@gmail.com>
2022-10-10 10:08:43 -0400

    fixed for loop in mounted

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -191,7 +191,7 @@ export default {
       invoke('read').then((res) => {
           const resArray = res.split('\n')
           const partsArray = resArray.split('=')
-          for(i in partsArray){
+          for(i=0; i<partsArray.length; i ++){
             store.commit('setConfig', partsArray[i], partsArray[i + 1])
           }
         })
