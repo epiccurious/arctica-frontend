64d7888394dce7d023ce92f4f8d4b37044cb0033
lappy <wildkard91@gmail.com>
2022-10-12 12:38:17 -0400

    removed make bitcoin dotfile from setup 1, redundant, added to init iso

--- src/views/setup/1.vue
+++ src/views/setup/1.vue
@@ -1,9 +1,5 @@
 <template>
-  <div v-if="this.loading == true">
-  <Loader/>
-  </div>
-
-  <div v-else class="login">
+  <div class="login">
     <header>
       <h1>Welcome to Arctica</h1>
       <h2>Let's begin the set up process.</h2>
@@ -15,29 +11,17 @@
 </template>
 
 <script>
-import Loader from '@/components/loader'
-const invoke = window.__TAURI__.invoke
 
 export default {
   name: 'Setup1',
-  components: {
-    Loader,
-  },
     methods: {
        acknowledge(){
-          //show loader
-          this.loading = true;
 
-          //create bitcoin dotfile on local machine
-          invoke('make_bitcoin_dotfile').then(() =>{
-            this.loading = false;
             this.$router.push({ name:'Setup2' })
-          })
         },
       },
     data(){
         return{
-          loading: false,
         }
     }
 }
