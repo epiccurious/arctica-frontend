1a4c92fb225c3c447157b156f88f2a89686fb83c
lappy <wildkard91@gmail.com>
2022-10-12 10:23:39 -0400

    removed test display from setup 29

--- src/views/setup/29.vue
+++ src/views/setup/29.vue
@@ -5,7 +5,6 @@
  <h2>Please remove DVD 2 and SD 2.</h2>
  <h2>Place SD card 2, and backups CD 2 and DVD 2 into Envelope 2.</h2> 
  <h2>This envelope should be stored somewhere secure and easily accessible, like your home safe.</h2>
- <h2>test: {{this.testVar}}</h2>
  </header>
       <div class="form_container">
         <form>
@@ -46,11 +45,6 @@ export default {
         return{
             checkbox: false,
             setupStep: '0',
-        }
-    },
-    computed:{
-        testVar(){
-            return store.getters.getTest
         }
     },
     mounted(){
