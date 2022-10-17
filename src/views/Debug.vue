5613e39f483d5e0150b22894f46e44eb17c3f751
lappy <wildkard91@gmail.com>
2022-10-17 10:37:49 -0400

    tweaks debug

--- src/views/Debug.vue
+++ src/views/Debug.vue
@@ -5,7 +5,8 @@
         <h2>This is for testing only.</h2>
     </header>
     <div class="btn_container"> 
-        <h2 v-for="item in this.test" :key="item">{{item}}</h2>
+        <h2>debug output:</h2>
+        <h2 v-for="item in this.test" :key="item">{{this.test.item}}</h2>
 
         <button @click="reboot()" class="btn">Reboot</button>
 
