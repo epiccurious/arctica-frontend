220d448acbbb5b35c6ca71617cda98ca6bb01048
wild-kard <wildkard91@gmail.com>
2022-11-04 19:39:48 -0400

    moved async write update setupstep variable 15 to setup step 26

--- src/views/setup/26.vue
+++ src/views/setup/26.vue
@@ -2,7 +2,7 @@
 <div class="page">
     <header>
         <h1>SD Card 2</h1>
-        <h2>Please power off this machine, insert SD card 2 and power on the machine.</h2>
+        <h2>Please power off this machine, remove SD 1 & insert SD 2 and power on the machine.</h2>
     </header> 
         <div class="btn_container"> 
             <button @click="acknowledge()" class="btn">Ok (this will close the application)</Button>
@@ -12,6 +12,9 @@
 </template>
 
 <script>
+import store from '../../store.js'
+const invoke = window.__TAURI__.invoke
+
 export default {
   name: 'Setup26',
     methods: {
@@ -22,8 +25,19 @@ export default {
             console.log('user trying to proceed without checkbox validation')
         },
     },
+    mounted(){
+        //update the setup step
+        invoke('async_write', {name: 'setupStep', value: this.setupStep}).then((res) => {
+            store.commit('setTest', `async writing setupstep ${this.setupStep}: ${res} `)
+            })
+            .catch((e) => {
+                store.commit('setTest', `async write error: ${e}`)
+            })
+    },
     data(){
-        return{}
+        return{
+            setupStep: 15,
+        }
     },
     }
 </script>
