4c5f0ca5cc8e26797ad5723c823485e7a609ab49
wild-kard <wildkard91@gmail.com>
2023-02-17 12:39:38 -0500

    completed refactor of recovery procedure

--- src/views/Manual_Login/Recovery_additional.vue
+++ src/views/Manual_Login/Recovery_additional.vue
@@ -1,14 +1,11 @@
 <template>
-  <div v-if="this.loading== true" class="page">
-    <header>
-      <h1>Manual Recovery In Progress.</h1>
-      <h2>Please Wait...</h2>
-    </header>
-  </div>
-
+  <div v-if="this.loading == true">
+  <Loader/>
+</div>
   <div v-else class="page">
     <header>
-      <h1>Please remove your recovery CD and reboot from your next available SD card</h1>
+      <h1>Manual Recovery</h1>
+      <h2>Please remove your recovery CD and reboot with your next available SD card</h2>
     </header>
 
   </div>
@@ -16,19 +13,18 @@
 
 <script>
 import store from '../../store.js'
+import Loader from '@/components/loader'
 const invoke = window.__TAURI__.invoke
 
 export default {
   name: 'RecoveryAdditional',
-    methods: {
-        acknowledge(){
-        },
-    },
+  components: {
+    Loader
+  },
     computed: {
       numberToRecover(){
         return store.getters.getNumberToRecover
       },
-      
     },
     mounted(){
       // collecting shards from $HOME on current SD and adding them to /mnt/ramdisk/CDROM/shards
@@ -39,9 +35,10 @@ export default {
         //calculate numbertorecover differential based on how many shards are present on recoverycd
         invoke('calculate_number_of_shards').then((res)=> {
           store.commit('setDebug', `calculating number of shards in ramdisk: number = ${res}`)
-          store.commit('setLoadMessage', 'refreshing Recovery CD...')
+
           //if numbertorecover threshold is not yet met
           if(this.numberToRecover > res){
+            store.commit('setLoadMessage', 'refreshing Recovery CD...')
             store.commit('setDebug', `Need more shards to complete recovery. obtained ${res} of ${this.numberToRecover}`)
             invoke('refresh_cd').then((res)=>{
               store.commit('setDebug', `refreshing recovery CD with the contents of /mnt/ramdisk/CDROM: ${res}`)
@@ -56,9 +53,34 @@ export default {
           //if the numbertorecover threshold is met
           else{
             store.commit('setDebug', `shard threshold met, obtained ${res} of ${this.numberToRecover}`)
-            //else{recoveryfinish: reconstitute masterkey, genisoimage, blank inserted disc, burn new iso}
-
+            store.commit('setLoadMessage', 'Reconstituting Masterkey...')
+            invoke('combine_shards').then((res)=>{
+              store.commit('setDebug', `reconstituting masterkey ${res}`)
+              store.commit('setLoadMessage', 'Converting CD Config...')
+              invoke('convert_to_transfer_cd').then((res)=>{
+                store.commit('setDebug', `converting CD config from recoverycd to transfercd ${res}`)
+                store.commit('setLoadMessage', 'Refreshing CD...')
+                invoke('refresh_cd').then((res)=>{
+                  store.commit('setDebug', `refreshing CD: ${res}`)
+                  this.loading = false
+                  this.$router.push({ name: 'RecoverySuccess' })
+                }) .catch((e)=>{
+                    store.commit('setDebug', `error refreshing cd: ${e}`)
+                    store.commit('setErrorMessage', `Error refreshing cd Error code: RecoveryAdditional3 Response: ${e}`)
+                    this.$router.push({ name: 'Error' })
+              }) .catch((e)=>{
+                store.commit('setDebug', `error converting cd: ${e}`)
+                store.commit('setErrorMessage', `Error converting cd Error code: RecoveryAdditional4 Response: ${e}`)
+                this.$router.push({ name: 'Error' })
+            }) .catch((e)=>{
+                store.commit('setDebug', `error combining shards: ${e}`)
+                store.commit('setErrorMessage', `Error combining shards Error code: RecoveryAdditional5 Response: ${e}`)
+                this.$router.push({ name: 'Error' })
+              })
+              })
+            })
           }
+          
       })
       .catch((e) => {
         store.commit('setDebug', `error calculating shards on recovery cd: ${e}`)
--- src/views/Manual_Login/Recovery_success.vue
+++ src/views/Manual_Login/Recovery_success.vue
@@ -1,67 +1,19 @@
 <template>
-<div v-if="this.loading == true">
-  <Loader/>
-</div>
 
-  <div v-else class="page">
+  <div class="page">
     <header>
       <h1>Manual Recovery Success.</h1>
-      <h2>You may now log in to your account without the use of a BPS.</h2>
+      <h2>You may now log in to your account without the use of passphrase.</h2>
       <h2>Your transfer CD will hold your masterkey for login. Keep it safe.</h2>
-      <h2>Please insert SD 1 and your transfer CD and reboot this machine.</h2>
+      <h2>Please remove your transfer CD, reboot into SD 1 and use your transfer CD to login.</h2>
     </header>
-    <div class="btn_container"> 
-        <button @click="acknowledge()" class="btn">Ok</Button>
-        <button @click="help()" class="btn2">I need help</button>
-    </div>
   </div>
 </template>
 
 <script>
-import store from '../../store.js'
-import Loader from '@/components/loader'
-const invoke = window.__TAURI__.invoke
 
 export default {
   name: 'RecoverySuccess',
-  components: {
-    Loader
-  },
-    methods: {
-        acknowledge(){
-        },
-        help(){
-        }
-    },
-    mounted(){
-      //copying recovery cd to ramdisk
-      invoke('copy_recovery_cd').then((res)=>{
-        store.commit('setDebug', `copying recovery cd ${res}`)
-        //combine shards into masterkey
-        invoke('combine_shards').then((res)=>{
-            store.commit('setDebug', `combining shards in ramdisk ${res}`)
-            //refresh cd with masterkey and a new config designator: transfercd
-            invoke('convert_to_transfer_cd').then((res)=>{
-              store.commit('setDebug', `converting cd to a transfer cd: ${res}`)
-              this.loading=false
-            })
-            .catch((e)=>{
-              store.commit('setDebug', `error converting to transfer cd ${e}`)
-            })
-          })
-          .catch((e)=>{
-            store.commit('setDebug', `error combining shards ${e}`)
-          })
-      })
-      .catch((e)=>{
-        store.commit('setDebug', `error copying recovery cd ${e}`)
-      })
-    },
-    data() {
-    return {
-    loading: true
-    }
-  },
   }
 </script>
 
