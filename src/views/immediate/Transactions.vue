a22e7c5c46f30e888498d5571e3a90a61282a04c
wild-kard <wildkard91@gmail.com>
2023-03-24 15:39:30 -0400

    testing

--- src/views/immediate/Transactions.vue
+++ src/views/immediate/Transactions.vue
@@ -60,10 +60,11 @@ export default {
     invoke('get_transactions', {wallet: "immediate", sdcard: this.sdCard.toString()}).then((res)=>{
                 //modify the json to remove the ListTransactionResult identifier
                 let modified = res.replace(/ListTransactionResult/g, '')
-                console.log(modified)
+                console.log(`modified ${modified}`)
                 store.commit('setDebug', `obtaining transaction history JSON for immediate wallet: ${modified}}`)
                 let arr = JSON.parse(modified)
-                console.log(arr)
+                console.log(`arr ${arr}`)
+                console.log(`time from tx index 0 ${arr[0].info.time}`)
                 store.commit('setImmediateTransactions', `${arr}`)
             })
             .catch((e)=>{
