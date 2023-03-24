6b8b8d190d15f2c9a3d5922d14db7db3cc51164d
wild-kard <wildkard91@gmail.com>
2023-03-24 13:43:45 -0400

    finally fixed the dumbest mistake in human history, balance now uses parsefloat rather than parseint

--- src/components/NavImmediate.vue
+++ src/components/NavImmediate.vue
@@ -83,10 +83,8 @@ export default{
             invoke('get_balance', {wallet: "immediate", sdcard: "1"}).then((res)=>{
                     store.commit('setDebug', `getting balance for immediate wallet: ${res}`)
                     console.log(`printing get_balance res: ${res}`)
-                    let bal = parseInt(res)
-                    let bal_pres = bal.toPrecision(9)
+                    let bal = parseFloat(res).toPrecision(9)
                     console.log(`printing get_balance parseint: ${bal}`)
-                    console.log(`printing bal precise: ${bal_pres}`)
                     store.commit('setImmediateBalance', `${bal}`)
                 }).catch((e)=>{
                 store.commit('setDebug', `error getting immediate wallet balance ${e}`)
