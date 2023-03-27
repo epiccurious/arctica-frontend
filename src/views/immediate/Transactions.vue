39367d3e556b829642aefefbd0bb50d860056603
wild-kard <wildkard91@gmail.com>
2023-03-27 16:02:33 -0400

    updates

--- src/views/immediate/Transactions.vue
+++ src/views/immediate/Transactions.vue
@@ -7,13 +7,13 @@
         </div>
         <div v-if="txHistory == false">
         <h2>You don't have any transactions yet.</h2></div>
-        <div v-else @click="transactionDetail(jsonData.info.txid)" v-for="transaction in this.jsonData" :key="transaction.info.blockhash ? transaction.info.blockhash: index" class="transaction_container">
+        <div v-else @click="transactionDetail(transaction.info.txid)" v-for="transaction in this.jsonData" :key="transaction.info.blockhash ? transaction.info.blockhash: index" class="transaction_container">
           <div class="transaction_container_left">
           <h2>{{ truncateString(transaction.detail.address) }}</h2>
-          <h3>{{ transaction.info.time }}</h3>
+          <h3>{{ (transaction.info.time * 1000).toLocaleString() }}</h3>
           </div>
           <div class="transaction_container_right">
-            <h2 class="balance">₿ {{ transaction.info.amount }} BTC</h2>
+            <h2 class="balance">₿ {{ transaction.detail.amount }} BTC</h2>
             <h3 class="fiat_currency"> $ 0.00</h3>
           </div>
         </div>
