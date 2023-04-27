13b0984426e8d84516f2848e390b9a9900b1fd73
wild-kard <wildkard91@gmail.com>
2023-04-27 12:08:50 -0400

    packup prior to stopping bitcoind on 1of2sucess

--- src/views/immediate/multi/1of2success.vue
+++ src/views/immediate/multi/1of2success.vue
@@ -1,5 +1,8 @@
 <template>
-<div class="page">
+<div v-if="this.loading == true">
+    <Loader/>
+</div>
+<div v-else class="page">
     <header>
         <h1>1 of 2 Signing Successful</h1>
         <h2>Please power off this machine. Swap HW 1 for HW 2 and power on this machine.</h2>
@@ -12,22 +15,37 @@
 
 <script>
 import store from '../../../store.js'
+import Loader from '@/components/loader'
 const invoke = window.__TAURI__.invoke
 
 export default {
   name: '1of2success',
   components: {
+    Loader,
     },
     computed:{
     },
     mounted(){
-        invoke('stop_bitcoind').then((res) =>{
-            store.commit('setDebug', `stopping bitcoin daemon ${res}`)
+        invoke('packup').then((res)=>{
+            store.commit('setDebug', `packing up sensitive ${res}`)
+            invoke('stop_bitcoind').then((res) =>{
+                store.commit('setDebug', `stopping bitcoin daemon ${res}`)
+                this.loading=false
+            }).catch((e)=>{
+                store.commit('setDebug', `error stopping bitcoin daemon: ${e}`)
+                store.commit('setErrorMessage', `Error with stopping bitcoin daemon Error Code: 1of2success-1 Response: ${e}`)
+                this.$router.push({ name:'Error' })
+            })
         }).catch((e)=>{
-            store.commit('setDebug', `error stopping bitcoin daemon: ${e}`)
-            store.commit('setErrorMessage', `Error with stopping bitcoin daemon Error Code: 1of2success-1 Response: ${e}`)
+            store.commit('setDebug', `error packing up sensitive ${res}`)
+            store.commit('setErrorMessage', `Error with packing up sensitive: 1of2success-2 Response: ${e}`)
             this.$router.push({ name:'Error' })
         })
+    }, 
+    data(){
+        return{
+            loading: true,
+        }
     }
 }
 </script>
