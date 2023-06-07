9a31a305b3730d0242a6489d7d012a73ca628352
wild-kard <wildkard91@gmail.com>
2023-06-07 13:24:32 -0400

    fixed an error

--- src/views/immediate/multi/broadcast.vue
+++ src/views/immediate/multi/broadcast.vue
@@ -75,8 +75,8 @@ export default {
                 invoke('decode_processed_psbt', {walletname: "immediate", hwnumber: this.currentHW.toString()}).then((res)=>{
                     //handle the condition where a user somehow got here with an incomplete PSBT
                     if(res.includes("ERROR PSBT not complete")){
-                        store.commit('setDebug', `error PSBT not fully signed ${e}`)
-                        store.commit('setErrorMessage', `Error PSBT not fully signed Error Code: immediateBroadcast-2 Response: ${e}`)
+                        store.commit('setDebug', `error PSBT not fully signed`)
+                        store.commit('setErrorMessage', `Error PSBT not fully signed Error Code: immediateBroadcast-2 Response: None`)
                         this.$router.push({ name:'Error' })
                     }else{ //expected path, return the tx info and display on front end
                         store.commit('setDebug', `decoded psbt: ${res}`)
