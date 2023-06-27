dc705e3fd91c421907ea1dc686186417d4756a8a
wild-kard <wildkard91@gmail.com>
2023-06-27 14:06:21 -0400

    added function to the receive page for the delayed wallet

--- src/views/delayed/Receive.vue
+++ src/views/delayed/Receive.vue
@@ -4,29 +4,27 @@
     <NavDelayed/>
     <div class="outer_container">
       <div class="receive_head_container">
-         <h1>Receive Bitcoin</h1>
-       </div>
-        <div class="receive_container">
-            <div class="receive_top">
-                <!-- <img src="@/assets/placeholderQR.png"> -->
-                
-                <h2 class="receive_address">Delayed wallet is not yet implemented</h2>
-            </div>
-            <div class="receive_bottom">
-                <div @click="copy(address)" id="copy">
-                    <img src="@/assets/Copy.png">
-                    <h2 class="copy_text">Copy Address</h2>
+        <h1>Receive Bitcoin</h1>
+             </div>
+            <div class="receive_container">
+                <div class="receive_top">
+                    <h2 class="receive_address">{{ address }}</h2>
                 </div>
-                <div id="download">
-                    <img src="@/assets/Arrow_down.png">
-                    <h2 class="download_text">Download QR Code</h2>
+                <div class="receive_bottom">
+                    <div @click="copy()" id="copy">
+                        <img src="@/assets/Copy.png">
+                        <h2 class="copy_text">Copy Address</h2>
+                    </div>
+                    <div @click="displayQR()" id="download">
+                        <img src="@/assets/Arrow_down.png">
+                        <h2 class="download_text">Display QR Code</h2>
+                    </div>
                 </div>
             </div>
-        </div>
-        <button @click="newAddress()" class="btn">New Address</Button>
-    </div>        
-</div>
-</template>
+            <button @click="newAddress()" class="btn">New Address</Button>
+        </div>        
+    </div>
+    </template>
 
 
 <!-- -download QR button not yet implemented  -->
@@ -35,7 +33,7 @@
 import NavDelayed from '@/components/NavDelayed'
 import DelayWarning from './DelayWarning'
 import store from '../../store.js'
-// const invoke = window.__TAURI__.invoke
+const invoke = window.__TAURI__.invoke
 
 export default {
   name: 'delayedReceive',
@@ -43,53 +41,99 @@ export default {
     NavDelayed,
     DelayWarning
   },
-  methods: {
-       async copy(s){
-          await navigator.clipboard.writeText(s)
-          alert('Copied Address!')
-        },
-        newAddress(){
-        //     invoke('get_address', {walletname: this.wallet, hwnumber:this.currentHW.toString()}).then((res)=>{
-        //     store.commit('setDebug', `getting new address for delayed wallet: ${res}`)
-        //     this.address = res
-        //   })
-        //   .catch((e)=>{
-        //     store.commit('setDebug', `error getting new immediate wallet address ${e}`)
-        //     store.commit('setErrorMessage', `Error getting new wallet address Error code: ImmediateReceive1 Response: ${e}`)
-        //     this.$router.push({ name: 'Error' })
-        //   })
-        },
-        ackWarning(){
-          this.warning = false
+  methods:{
+            copy(){
+            var copyText = document.getElementsByClassName("receive_address")[0];
+            console.log(copyText)
+            copyText.select();
+            navigator.clipboard.writeText(copyText.value);
+            alert('Copied Address!');
+            },
+            newAddress(){            
+                invoke('get_address', {walletname: this.wallet, hwnumber:this.currentHW.toString()}).then((res)=>{
+                store.commit('setDebug', `getting new address for delayed wallet: ${res}`)
+                this.address = res
+            })
+              .catch((e)=>{
+                store.commit('setDebug', `error getting new delayed wallet address ${e}`)
+                store.commit('setErrorMessage', `Error getting new wallet address Error code: DelayedReceive1 Response: ${e}`)
+                this.$router.push({ name: 'Error' })
+              })
+            },
+            displayQR(){
+              invoke('display_qr').then((res)=>{
+                store.commit('setDebug', `displaying QR code: ${res}`)
+              }).catch((e)=>{
+                store.commit('setDebug', `error getting new delayed wallet address ${e}`)
+                store.commit('setErrorMessage', `Error displaying QR code Error code: DelayedReceive2 Response: ${e}`)
+                this.$router.push({ name: 'Error' })
+              })
+            }
+    
       },
+      data(){
+          return{
+              address: null,
+              wallet: "delayed"
+          }
       },
-  data(){
-      return{
-          address: null,
-          wallet: "delayed",
-          warning: null
+      computed: {
+        currentHW(){
+            return store.getters.getcurrentHW
+          },
+      },
+    
+      mounted(){
+        invoke('get_address', {walletname: this.wallet, hwnumber:this.currentHW.toString()}).then((res)=>{
+                store.commit('setDebug', `getting new address for delayed wallet: ${res}`)
+                this.address = res
+            })
+              .catch((e)=>{
+                store.commit('setDebug', `error getting new delayed wallet address ${e}`)
+                store.commit('setErrorMessage', `Error getting new wallet address Error code: DelayedReceive3 Response: ${e}`)
+                this.$router.push({ name: 'Error' })
+              })
       }
-  },
-  computed:{
-    currentHW(){
-        return store.getters.getcurrentHW
-      },
-  },
-   mounted(){
-    // invoke('get_address', {walletname: this.wallet, hwnumber: this.currentHW.toString()}).then((res)=>{
-    //         store.commit('setDebug', `getting new address for immediate wallet: ${res}`)
-    //         this.address = res
-    //       })
-    //       .catch((e)=>{
-    //         store.commit('setDebug', `error getting new immediate wallet address ${e}`)
-    //         store.commit('setErrorMessage', `Error getting new wallet address Error code: ImmediateReceive1 Response: ${e}`)
-    //         this.$router.push({ name: 'Error' })
-    //       })
-    this.timeLock = store.getters.getTimeLock
-    this.address = store.getters.getDelayedAddress
-    if(this.timeLock == true){
-        this.warning = true
-    }else{this.warning = false}
-   },
-}
-</script>
+    
+    }
+            
+    </script>
+
+
+
+
+
+
+
+<template>
+    <div class="page">
+        <NavImmediate/>
+        <div class="outer_container">
+            <div class="receive_head_container">
+             <h1>Receive Bitcoin</h1>
+             </div>
+            <div class="receive_container">
+                <div class="receive_top">
+                    <h2 class="receive_address">{{ address }}</h2>
+                </div>
+                <div class="receive_bottom">
+                    <div @click="copy()" id="copy">
+                        <img src="@/assets/Copy.png">
+                        <h2 class="copy_text">Copy Address</h2>
+                    </div>
+                    <div @click="displayQR()" id="download">
+                        <img src="@/assets/Arrow_down.png">
+                        <h2 class="download_text">Display QR Code</h2>
+                    </div>
+                </div>
+            </div>
+            <button @click="newAddress()" class="btn">New Address</Button>
+        </div>        
+    </div>
+    </template>
+    
+    
+
+      
+    
+    
