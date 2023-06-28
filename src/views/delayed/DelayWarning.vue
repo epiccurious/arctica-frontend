b4185eb7bd0ea19b726a985adf61996e2b24fb94
wild-kard <wildkard91@gmail.com>
2023-06-28 10:58:51 -0400

    fixed some errors with delay warning, added a fallback condition to handle a case where the delayed wallet expires while the user is not at the dashboard

--- src/views/delayed/DelayWarning.vue
+++ src/views/delayed/DelayWarning.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="page">
+    <!-- timelock condition is true, expected behavior -->
+    <div v-if="timelock == true" class="page">
         <header>
             <h1>Warning! Your Delayed Wallet is currently timelocked.</h1>
             <h2>Your Delayed Wallet is timelocked for... <br><span class="strong_text">{{ this.delayedYears }} year(s), {{ this.delayedMonths }} month(s), {{ this.delayedDays }} day(s), {{ this.delayedHours }} hour(s), {{ this.delayedMinutes }} minute(s), {{ this.delayedSeconds }} second(s)</span></h2> 
             <h2>Accessing these funds early will involve significant cost.</h2> 
             <h2>For more information, read the <a class="blue_link" href="#">Time Machine Protocol</a>.</h2>
-        </header>
-        
+        </header>        
         <div class="form_container">
             <form>
                 <div class="lg_checkbox_container">
@@ -20,9 +20,23 @@
             </div>
         </div> 
     </div>
+    <!-- timelock condition is false, can only happen if the delayed wallet unlocks while user is within the wallet UI -->
+    <div v-else class="page">
+        <header>
+            <h1>Your Delayed Wallet has unlocked!.</h1>
+        </header>        
+        <div class="form_container">
+            <div class="btn_container"> 
+                <button @click="$emit('ackWarning')" class="btn">Proceed</Button>
+            </div>
+        </div> 
+    </div>
 </template>
 
 <script>
+import store from '../store.js'
+const invoke = window.__TAURI__.invoke
+
 
 export default {
     name: 'DelayWarning',
@@ -43,13 +57,22 @@ export default {
         warn(){
         },
     },
+    computed:{
+        timelock:{
+            get(){
+                return store.getters.getTimeLock
+            },
+            set(newVal){
+                store.commit('setTimeLock', newVal)
+            }
+        },
+    },
     mounted(){
         //calculate delayed_decay1
         invoke('calculate_decay_time', {file: "delayed_decay1"}).then((res)=>{
           console.log("delayed decay response:", res)
           if(res.includes("decay complete")){
             store.commit('setTimeLock', false)
-            $emit('ackWarning')
           }
           else{
             const parts = res.split(",")
